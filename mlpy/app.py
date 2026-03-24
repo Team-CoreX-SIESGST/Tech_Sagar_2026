b6f299e58f56e7f023cd9738fbc75d8a42df6d63
suthakaranburaj <suthakaranburaj@gmail.com>
2026-03-24 05:40:14 +0530

    cc

--- mlpy/app.py
+++ mlpy/app.py
@@ -1,2 +1,13 @@
+import os
+
+import uvicorn
+
 from backend.main import app
 
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+    )
